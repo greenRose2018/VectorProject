3e488d5dc195724bf2b46acc02b640c9d0d8c333
greenRose2018 <brifun13@gmail.com>
2018-01-29 09:05:00 -0700

    setup project with runner and controller

---
+++ VectorProject/Controller/Controller.hpp
@@ -0,0 +1,20 @@
+//
+//  Controller.hpp
+//  VectorProject
+//
+//  Created by Morales, Brittney on 1/29/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <iostream>
+
+class Controller
+{
+    Controller();
+    void start();
+};
+
+#endif /* Controller_hpp */
---
+++ VectorProject/Controller/Runner.cpp
@@ -0,0 +1,15 @@
+//
+//  Runner.cpp
+//  VectorProject
+//
+//  Created by Morales, Brittney on 1/29/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+int main()
+{
+    Controller myVectorApp;
+    myVectorApp.start();
+};
