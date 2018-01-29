5dbf099a843fb19290c4d628164ad044b63f4abc
greenRose2018 <brifun13@gmail.com>
2018-01-29 10:06:04 -0700

    made vectors in String and int both work

--- VectorProject/Controller/Controller.cpp
+++ VectorProject/Controller/Controller.cpp
@@ -23,13 +23,39 @@ void Controller:: start()
 
 string Controller:: strVector()
 {
+    vector<string> vStr;
+    vStr = {"Yokoso","welcome","Bonjour","Huanying","Bienvenidos"};
     
+    for(int i = 0; i < vStr.size(); i++)
+    {
+        cout << "Index is: " << i << ". Inside this is: " << vStr[i] << endl;
+    }
     
     return "0";
 }
 
 int Controller:: intVector()
 {
+    vector<int> vInt;
+    vInt = {10,100,90,16,45,85,94};
     
+    for (int i = 0; i <  vInt.size(); i++)
+    {
+        cout << "Index is: " << i << ". Inside this is: " << vInt[i] << endl;
+    }
+    vInt.push_back(98765);
+    cout << "The size of my vector is: " << vInt.size() << endl;
+    for (int i = 0; i <  vInt.size(); i++)
+    {
+        cout << "Index is: " << i << ". Inside this is: " << vInt[i] << endl;
+    }
+    
+    vInt.pop_back();
+    cout << "The size of my vector is: " << vInt.size() << endl;
+    
+    for (int i = 0; i <  vInt.size(); i++)
+    {
+        cout << "Index is: " << i << ". Inside this is: " << vInt[i] << endl;
+    }
     return 1;
 }
